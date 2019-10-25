f45fefefb3812047066b9ed768f1ac43b7ab5cb9
lea <leatraccoen@gmail.com>
2019-10-25 12:05:28 +0200

    css grid bien fait

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,18 @@
-$('.anchor-scroll').anchorScroll({
-    scrollSpeed: 800, // scroll speed
-    offsetTop: 0, // offset for fixed top bars (defaults to 0)
-    onScroll: function () {
-        // callback on scroll start
-    },
-    scrollEnd: function () {
-        // callback on scroll end
-    }
+
+$(document).ready(function(){
+    // au clic sur un lien
+    $('a').on('click', function(evt){
+        // bloquer le comportement par défaut: on ne rechargera pas la page
+        evt.preventDefault();
+        // enregistre la valeur de l'attribut  href dans la variable target
+        var target = $(this).attr('href');
+        /* le sélecteur $(html, body) permet de corriger un bug sur chrome
+        et safari (webkit) */
+        $('html, body')
+        // on arrête toutes les animations en cours
+            .stop()
+            /* on fait maintenant l'animation vers le haut (scrollTop) vers
+             notre ancre target */
+            .animate({scrollTop: $(target).offset().top}, 1000 );
+    });
 });
