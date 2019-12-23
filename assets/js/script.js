4b1e2dc73fe58768f7c8706edcd9d616bfcd01a0
lea <leatraccoen@gmail.com>
2019-12-23 14:39:44 +0100

    supp files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,14 @@
+$(document).ready(function () {
+    $(".ham").click(function () {
+        $('.main-container').hide();
+        $(".fas").hide();
+        if ($("nav").first().is(":hidden")) {
+            $("nav").show();
 
-$(document).ready(function(){
-    // au clic sur un lien
-    $('a').on('click', function(evt){
-        // bloquer le comportement par défaut: on ne rechargera pas la page
-        evt.preventDefault();
-        // enregistre la valeur de l'attribut  href dans la variable target
-        var target = $(this).attr('href');
-        /* le sélecteur $(html, body) permet de corriger un bug sur chrome
-        et safari (webkit) */
-        $('html, body')
-        // on arrête toutes les animations en cours
-            .stop()
-            /* on fait maintenant l'animation vers le haut (scrollTop) vers
-             notre ancre target */
-            .animate({scrollTop: $(target).offset().top}, 1000 );
+        } else {
+            $("nav").slideUp();
+            $(".fas").show();
+            $('.main-container').show();
+        }
     });
 });
